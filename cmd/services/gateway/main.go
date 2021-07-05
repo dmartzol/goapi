f6d9ecfe74c4b0962be599a31ee3e2df74af3f11
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-07-05 02:45:05 -0500

    wip

--- cmd/services/gateway/main.go
+++ cmd/services/gateway/main.go
@@ -9,17 +9,6 @@ import (
 	"go.uber.org/zap"
 )
 
-const (
-	Ldate         = 1 << iota                  // the date in the local time zone: 2009/01/23
-	Ltime                                      // the time in the local time zone: 01:23:23
-	Lmicroseconds                              // microsecond resolution: 01:23:23.123123.  assumes Ltime.
-	Llongfile                                  // full file name and line number: /a/b/c/d.go:23
-	Lshortfile                                 // final file name element and line number: d.go:23. overrides Llongfile
-	LUTC                                       // if Ldate or Ltime is set, use UTC rather than the local time zone
-	Lmsgprefix                                 // move the "prefix" from the beginning of the line to before the message
-	LstdFlags     = Ldate | Ltime | Lshortfile // initial values for the standard logger
-)
-
 func main() {
 	l, err := zap.NewProduction()
 	if err != nil {
@@ -28,7 +17,6 @@ func main() {
 	defer l.Sync()
 	logger := l.Sugar()
 
-	log.SetFlags(LstdFlags)
 	dbClient, err := postgres.NewDBClient()
 	if err != nil {
 		log.Fatalf("error initializing database: %+v", err)
