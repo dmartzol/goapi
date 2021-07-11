10a67dfe01d66efac35f57f8131d63c1c5bcaa74
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-07-10 22:37:11 -0500

    wip

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"database/sql"
 	"errors"
-	"log"
 	"net/http"
 
 	"github.com/dmartzol/api-template/internal/storage/postgres"
@@ -25,23 +24,23 @@ func (h *handler) AuthMiddleware(next http.Handler) http.Handler {
 		}
 		c, err := r.Cookie(CookieName)
 		if err != nil {
-			log.Printf("AuthMiddleware ERROR getting cookie: %+v", err)
+			h.Errorw("could not fetch cookie", "cookie", CookieName, "error", err)
 			httpresponse.RespondJSONError(w, "", http.StatusUnauthorized)
 			return
 		}
 		s, err := h.db.UpdateSession(c.Value)
 		if err != nil {
+			h.Errorw("could not update session", "token", c.Value, "error", err)
 			if err == sql.ErrNoRows {
-				log.Printf("AuthMiddleware ERROR unable to find session %s: %+v", c.Value, err)
+				h.Errorw("unable to find session", "error", err)
 				httpresponse.RespondJSONError(w, "", http.StatusUnauthorized)
 				return
 			}
 			if errors.Is(err, postgres.ErrExpiredSession) {
-				h.Errorw("expired session", "session", c.Value, "error", err)
+				h.Errorw("expired session", "error", err)
 				httpresponse.RespondJSONError(w, "", http.StatusUnauthorized)
 				return
 			}
-			log.Printf("AuthMiddleware ERROR for session %s: %+v", c.Value, err)
 			httpresponse.RespondJSONError(w, "", http.StatusInternalServerError)
 			return
 		}
