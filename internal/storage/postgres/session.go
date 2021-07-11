fff4380b6fcab25d9a03fd26c6829859fce2931c
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-07-10 21:35:05 -0500

    wip

--- internal/storage/postgres/session.go
+++ internal/storage/postgres/session.go
@@ -1,12 +1,12 @@
 package postgres
 
 import (
-	"log"
 	"time"
 
 	models "github.com/dmartzol/api-template/internal/model"
 	"github.com/google/uuid"
 	_ "github.com/lib/pq"
+	"github.com/pkg/errors"
 )
 
 var (
@@ -18,10 +18,7 @@ func (db *DB) SessionFromToken(token string) (*models.Session, error) {
 	var s models.Session
 	sqlStatement := `select * from sessions where token = $1`
 	err := db.Get(&s, sqlStatement, token)
-	if err != nil {
-		return nil, err
-	}
-	return &s, nil
+	return &s, err
 }
 
 // CreateSession creates a new session
@@ -34,33 +31,10 @@ func (db *DB) CreateSession(accountID uuid.UUID) (*models.Session, error) {
 
 // ExpireSessionFromToken expires the session with the given token
 func (db *DB) ExpireSessionFromToken(token string) (*models.Session, error) {
-	tx, err := db.Beginx()
-	if err != nil {
-		return nil, err
-	}
 	var s models.Session
 	sqlStatement := `update sessions set expiration_time = current_timestamp where token = $1 returning *`
-	err = tx.Get(&s, sqlStatement, token)
-	if err != nil {
-		tx.Rollback()
-		return nil, err
-	}
-	return &s, tx.Commit()
-}
-
-// CleanSessionsOlderThan deletes all sessions older than age(in seconds) and returns the number of rows affected
-func (db *DB) CleanSessionsOlderThan(age time.Duration) (int64, error) {
-	t := time.Now().Add(-age * time.Second)
-	sqlStatement := `delete from sessions where last_activity_time < $1`
-	res, err := db.Exec(sqlStatement, t)
-	if err != nil {
-		return -1, err
-	}
-	count, err := res.RowsAffected()
-	if err != nil {
-		return -1, err
-	}
-	return count, nil
+	err := db.Get(&s, sqlStatement, token)
+	return &s, err
 }
 
 // UpdateSession updates a session in the db with the current timestamp
@@ -73,9 +47,8 @@ func (db *DB) UpdateSession(token string) (*models.Session, error) {
 	sqlStatement := `select * from sessions where token = $1`
 	tx.Get(&session, sqlStatement, token)
 	if err != nil {
-		log.Printf("UpdateSession db - ERROR fetching session from token %s: %+v", token, err)
 		tx.Rollback()
-		return nil, err
+		return nil, errors.Wrapf(err, "error fetching session from token %s", token)
 	}
 	if session.ExpirationTime.Before(time.Now()) {
 		return nil, ErrExpiredResource
@@ -84,9 +57,8 @@ func (db *DB) UpdateSession(token string) (*models.Session, error) {
 	sqlStatement = `update sessions set last_activity_time=default where token = $1 returning *`
 	err = tx.Get(&updatedSession, sqlStatement, token)
 	if err != nil {
-		log.Printf("UpdateSession db - ERROR updating session from token %s: %+v", token, err)
 		tx.Rollback()
-		return nil, err
+		return nil, errors.Wrapf(err, "error updating session from token %s", token)
 	}
 	return &updatedSession, tx.Commit()
 }
