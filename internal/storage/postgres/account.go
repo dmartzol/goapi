3334841f46051b60111476200e8eeccea4a0d45a
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-07-10 21:23:56 -0500

    wip

--- internal/storage/postgres/account.go
+++ internal/storage/postgres/account.go
@@ -3,10 +3,10 @@ package postgres
 import "github.com/dmartzol/api-template/internal/model"
 
 // AccountWithCredentials returns an account if the email and password provided match an (email,password) pair in the db
-func (db *DB) AccountWithCredentials(email, allegedPassword string) (*model.Account, error) {
+func (db *DB) AccountWithCredentials(email, password string) (*model.Account, error) {
 	var a model.Account
 	sqlStatement := `select * from accounts a where a.email = $1 and a.passhash = crypt($2, a.passhash)`
-	err := db.Get(&a, sqlStatement, email, allegedPassword)
+	err := db.Get(&a, sqlStatement, email, password)
 	if err != nil {
 		return nil, err
 	}
