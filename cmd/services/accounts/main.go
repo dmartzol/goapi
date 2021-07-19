c1a6f3c042d8b03cf01357a272a0e5a4f71a25dd
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-07-18 19:01:45 -0500

    wip

--- cmd/services/accounts/main.go
+++ cmd/services/accounts/main.go
@@ -1,51 +1,41 @@
 package main
 
 import (
+	"context"
+	"log"
+	"net"
+
 	pb "github.com/dmartzol/api-template/internal/protos"
 	"github.com/dmartzol/api-template/internal/storage/postgres"
+	"google.golang.org/grpc"
 )
-
-// server is used to implement helloworld.GreeterServer.
-// type accountsService struct {
-// 	pb.UnimplementedGreeterServer
-// }
 
 type accountService struct {
 	pb.UnimplementedAccountsServer
 	*postgres.DB
 }
 
-// func (s *accountService) Account(ctx context.Context, accID *protoTemplate.AccountRequest) (*protoTemplate.AccountMessage, error) {
-// 	a, err := s.DB.AccountWithCredentials("", "")
-// 	return a, err
-// }
-
-// func main() {
-// 	log.Fatal("")
-// 	a := model.Account{}
-// 	lis, err := net.Listen("tcp", port)
-// 	if err != nil {
-// 		log.Fatalf("failed to listen: %v", err)
-// 	}
-// 	s := grpc.NewServer()
-// 	pb.RegisterGreeterServer(s, &accountsService{})
-
-// 	if err := s.Serve(lis); err != nil {
-// 		log.Fatalf("failed to serve: %v", err)
-// 	}
-// }
+func (s *accountService) Account(ctx context.Context, accID *pb.AccountRequest) (*pb.AccountMessage, error) {
+	a, err := s.DB.AccountWithCredentials("", "")
+	log.Printf("acc %+v", a)
+	return nil, err
+}
 
 func main() {
-	// a := template.ThisConst
-	// log.Printf("my constant %s", a)
-
-	// dbClient, err := postgres.NewDBClient()
-	// if err != nil {
-	// 	log.Fatalf("error initializing database: %+v", err)
-	// }
-	// lis, err := net.Listen("tcp", ":50051")
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-	// s := grpc.NewServer()
+	dbClient, err := postgres.NewDBClient()
+	if err != nil {
+		log.Fatalf("error initializing database: %+v", err)
+	}
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	aS := accountService{
+		DB: dbClient,
+	}
+	pb.RegisterAccountsServer(s, &aS)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %+v", err)
+	}
 }
--- config/postgres/create_tables.sql
+++ config/postgres/create_tables.sql
@@ -14,16 +14,8 @@ CREATE TABLE accounts (
     last_name VARCHAR NOT NULL,
     dob date NOT NULL,
     gender CITEXT DEFAULT NULL,
-    active BOOLEAN NOT NULL DEFAULT FALSE CHECK ((confirmed_email AND review_time IS NOT NULL) OR NOT active),
     email CITEXT NOT NULL UNIQUE,
-    confirmed_email BOOLEAN DEFAULT FALSE,
-    phone_number VARCHAR UNIQUE DEFAULT NULL,
-    confirmed_phone BOOLEAN DEFAULT FALSE,
-    passhash TEXT NOT NULL,
-    failed_logins_count INT DEFAULT 0,
-    door_code VARCHAR DEFAULT NULL,
-    external_payment_customer_id INT DEFAULT NULL CHECK ((confirmed_email and review_time IS NOT NULL) OR external_payment_customer_id IS NULL),
-    review_time TIMESTAMPTZ DEFAULT NULL,
+    pass_hash TEXT NOT NULL,
     created_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_time TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
