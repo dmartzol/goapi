4c5fa71b517f397567d7fadbea226c650e6f8508
Daniel Martinez Olivas <danielmartinezolivas@gmail.com>
2021-09-25 19:55:53 -0500

    removes unnecessary logging and renames variable

--- cmd/services/gateway/service/service.go
+++ cmd/services/gateway/service/service.go
@@ -24,7 +24,6 @@ func New(structuredLogging, verbose bool) (*gatewayService, error) {
 	if err != nil {
 		return nil, errors.Wrap(err, "failed to create logger")
 	}
-	logger.Info("creating grcp connection")
 	accountsAddres := accountsHost + ":" + accountservice.Port
 	opts := []grpc.DialOption{grpc.WithInsecure()}
 	conn, err := grpc.Dial(accountsAddres, opts...)
@@ -32,16 +31,14 @@ func New(structuredLogging, verbose bool) (*gatewayService, error) {
 		return nil, errors.Wrap(err, "failed to create connection")
 	}
 	// defer conn.Close()
-	logger.Info("creating accounts service client")
 	accountsClient := pb.NewAccountsClient(conn)
-	apiHandler, err := handler.New(accountsClient, logger, verbose)
+	handler, err := handler.New(accountsClient, logger, verbose)
 	if err != nil {
 		log.Panicf("error creating handler: %v", err)
 	}
-	logger.Info("initializing routes")
-	apiHandler.InitializeRoutes()
+	handler.InitializeRoutes()
 	s := gatewayService{
-		Handler: apiHandler,
+		Handler: handler,
 	}
 
 	// ctx, cancel := context.WithTimeout(context.Background(), time.Second)
